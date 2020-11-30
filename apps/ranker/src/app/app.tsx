829389ab8d451f565df8968a9bc2c0ccda2e9918
Zarar Siddiqi <zarars@gmail.com>
2020-11-30 16:29:43 -0500

    CSS reset

--- apps/ranker/src/app/app.tsx
+++ apps/ranker/src/app/app.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ValidationFileUpload } from './validation-file-upload'
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import {
   BrowserRouter as Router,
@@ -7,14 +9,31 @@ import {
   Route,
   Link
 } from "react-router-dom";
+// Import the wrapper component, and the the creator function
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  }
+});
+
 
 export const App = () => {
   return (
     <Router>
-      <Link to={`/validation-file-upload`}>Upload Validation File</Link>
-      <Switch>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline/>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Link to={`/validation-file-upload`}>Upload Validation File</Link>
+        </Grid>
+      </Grid>
+      <Grid>
+        <Switch>
           <Route path="/validation-file-upload" component={ValidationFileUpload}/>
-      </Switch>
+        </Switch>
+      </Grid>
+      </MuiThemeProvider>
     </Router>
   );
 };
