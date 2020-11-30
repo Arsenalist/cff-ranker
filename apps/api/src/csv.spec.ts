b7d53f15a6b80517a3097d87fad6eb88b58befff
Zarar Siddiqi <zarars@gmail.com>
2020-11-29 20:37:18 -0500

    CSV tests

---
+++ apps/api/src/csv.spec.ts
@@ -0,0 +1,21 @@
+import { parseValidationFileContents } from './csv';
+
+describe('csv parsing', () => {
+  beforeEach(() => {
+  });
+  it('parses column names correctly', async () => {
+    const contents = "Surname,Name,YOB,Gender,Club,Branch,Country,CFF#,Validated\n" +
+                     "Lowry,Kyle,1986,M,TOR,ON,CAN,123,y";
+    const result = await parseValidationFileContents(contents);
+    expect(result.length).toBe(1);
+    const row = result[0];
+    expect(row.surname).toBe("Lowry");
+    expect(row.name).toBe("Kyle");
+    expect(row.yearOfBirth).toBe("1986");
+    expect(row.club).toBe("TOR");
+    expect(row.branch).toBe("ON");
+    expect(row.country).toBe("CAN");
+    expect(row.cffNumber).toBe("123");
+    expect(row.validated).toBe("y");
+  });
+});
