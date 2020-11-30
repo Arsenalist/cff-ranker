579b3140e00fd4e444fd7ea84853cdeed43f181b
Zarar Siddiqi <zarars@gmail.com>
2020-11-29 20:37:39 -0500

    Test environment is node for API

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   displayName: 'api',
   preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
