df300cc6d295a97c3e1b889f0f3d8bc50dd71de6
Zarar Siddiqi <zarars@gmail.com>
2020-12-01 12:55:42 -0500

    FFF format parser

--- apps/api/src/csv.spec.ts
+++ apps/api/src/csv.spec.ts
@@ -1,9 +1,9 @@
-import { parseValidationFileContents } from './csv';
+import { parseValidationFileContents, parseCompetitionFileContents } from './csv';
 
-describe('csv parsing', () => {
+describe('validation file csv parsing', () => {
   beforeEach(() => {
   });
-  it('parses column names correctly', async () => {
+  it('parses column names for validation file correctly', async () => {
     const contents = "Surname,Name,YOB,Gender,Club,Branch,Country,CFF#,Validated\n" +
                      "Lowry,Kyle,1986,M,TOR,ON,CAN,123,y";
     const result = await parseValidationFileContents(contents);
@@ -17,5 +17,38 @@ describe('csv parsing', () => {
     expect(row.country).toBe("CAN");
     expect(row.cffNumber).toBe("123");
     expect(row.validated).toBe("y");
+  });
+});
+
+describe('competition file csv parsing', () => {
+  const csv = "FFF;WIN;competition;sylvie clement;individuel\n" +
+              "10/12/2011;fleuret;M;senior;FM CHALLENGE DE LA VILLE DE LONGUEUIL;FM OM\n" +
+              "TEISSEIRE,Nicolas,1986,M,CAN,,;,,;C06-0516,QC,OM,,;1,t\n" +
+              "VANHAASTER,Maximilien,1992,M,CAN,,;,,;C06-0019,QC,CRA,,;2,t\n" +
+              "BRODEUR,Marc-Antoine,1993,M,CAN,,;,,;C06-0999,QC,OM,,;3,t"
+
+  it('parse headers', async () => {
+    const result = await parseCompetitionFileContents(csv);
+    expect(result.creator).toBe("sylvie clement")
+    expect(result.competitionType).toBe("individuel")
+    expect(result.competitionDate).toBe("10/12/2011")
+    expect(result.weapon).toBe("fleuret")
+    expect(result.gender).toBe("M")
+    expect(result.level).toBe("senior")
+    expect(result.tournamentName).toBe("FM CHALLENGE DE LA VILLE DE LONGUEUIL")
+    expect(result.tournamentLocation).toBe("FM OM")
+  });
+  it('parse player results', async () => {
+    const result = await parseCompetitionFileContents(csv);
+    expect(result.results[0].surname).toBe("TEISSEIRE")
+    expect(result.results[0].name).toBe("Nicolas")
+    expect(result.results[0].yearOfBirth).toBe("1986")
+    expect(result.results[0].gender).toBe("M")
+    expect(result.results[0].country).toBe("CAN")
+    expect(result.results[0].cffNumber).toBe("C06-0516")
+    expect(result.results[0].branch).toBe("QC")
+    expect(result.results[0].club).toBe("OM")
+    expect(result.results[0].rank).toBe("1")
+    expect(result.results[0].figureThisOut).toBe("t")
   });
 });
--- apps/api/src/csv.ts
+++ apps/api/src/csv.ts
@@ -19,4 +19,55 @@ async function parseValidationFileContents(fileContents) {
   });
 }
 
-export { parseValidationFileContents }
+async function parseCompetitionFileContents(fileContents) {
+  const { line1Values, line2Values } = parseHeaderRows(fileContents);
+  const records = await parseResults(fileContents);
+
+  return (
+    {
+      creator: line1Values[3],
+      competitionType: line1Values[4],
+      competitionDate: line2Values[0],
+      weapon: line2Values[1],
+      gender: line2Values[2],
+      level: line2Values[3],
+      tournamentName: line2Values[4],
+      tournamentLocation: line2Values[5],
+      results: records
+    }
+  )
+}
+
+function parseHeaderRows(fileContents) {
+  const lines = fileContents.split(/\r?\n/);
+  const line1Values = lines[0].split(/;/);
+  const line2Values = lines[1].split(/;/);
+  return { line1Values, line2Values };
+}
+
+async function parseResults(fileContents) {
+  const records = await csv.parse(fileContents, {
+    from_line: 3,
+    delimiter: ';',
+    on_record: (record, { lines }) => {
+      const block1 = record[0].split(/,/);
+      const block3 = record[2].split(/,/);
+      const block4 = record[3].split(/,/);
+      return {
+        surname: block1[0],
+        name: block1[1],
+        yearOfBirth: block1[2],
+        gender: block1[3],
+        country: block1[4],
+        cffNumber: block3[0],
+        branch: block3[1],
+        club: block3[2],
+        rank: block4[0],
+        figureThisOut: block4[1]
+      };
+    }
+  });
+  return records;
+}
+
+export { parseValidationFileContents, parseCompetitionFileContents }
