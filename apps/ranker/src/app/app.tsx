1e24c61ce102c0dd3d5a916564f38be6d4aab40a
Zarar Siddiqi <zarars@gmail.com>
2020-12-03 15:56:14 -0500

    Better menu

--- apps/ranker/src/app/app.tsx
+++ apps/ranker/src/app/app.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { ValidationFileUpload } from './validation-file-upload'
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-// Import the wrapper component, and the the creator function
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
-import { CloudUpload } from '@material-ui/icons';
-import logo from '../assets/cff-logo.png'
-import { Divider } from '@material-ui/core';
-import {
-  ClassificationUploadPage,
-  CompetitionUploadPage,
-  ValidateFileUploadPage,
-  ViewRankingsPage
-} from './pages';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { createMuiTheme, makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import { ClassificationUploadPage, CompetitionUploadPage, ValidateFileUploadPage, ViewRankingsPage } from './pages';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar/Toolbar';
 
 const theme = createMuiTheme({
   typography: {
@@ -32,44 +18,69 @@ const theme = createMuiTheme({
   }
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export const App = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Router>
       <MuiThemeProvider theme={theme}>
         <CssBaseline/>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <img src={logo} alt={"Logo"}/>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton}  onClick={handleMenu} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                CFF
+              </Typography>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose} component={Link} to="/validation-file-upload">Upload Validation File</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/competition-file-upload">Upload Competition File</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/classification-file-upload">Upload Classification File</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/view-rankings">View Rankings</MenuItem>
+              </Menu>
+            </Toolbar>
+          </AppBar>
         </Grid>
-        <Grid item xs={2}>
-          <Divider />
-          <List>
-              <ListItem button component={Link} to="/validation-file-upload">
-                <ListItemIcon> <CloudUpload /></ListItemIcon>
-                <ListItemText primary="Upload Validation File" />
-              </ListItem>
-          </List>
-          <List>
-            <ListItem button component={Link} to="/competition-file-upload">
-              <ListItemIcon> <CloudUpload /></ListItemIcon>
-              <ListItemText primary="Upload Competition File" />
-            </ListItem>
-          </List>
-          <List>
-            <ListItem button component={Link} to="/classification-file-upload">
-              <ListItemIcon> <CloudUpload /></ListItemIcon>
-              <ListItemText primary="Upload Classification File" />
-            </ListItem>
-          </List>
-          <List>
-            <ListItem button component={Link} to="/view-rankings">
-              <ListItemIcon> <FormatListNumberedIcon /></ListItemIcon>
-              <ListItemText primary="View Rankings" />
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item xs={10}>
+        <Grid item xs={2}/>
+        <Grid item xs={8}>
           <Switch>
             <Route path="/validation-file-upload" component={ValidateFileUploadPage} />
             <Route path="/competition-file-upload" component={CompetitionUploadPage} />
@@ -77,6 +88,7 @@ export const App = () => {
             <Route path="/view-rankings" component={ViewRankingsPage} />
           </Switch>
         </Grid>
+        <Grid item xs={2}/>
       </Grid>
       </MuiThemeProvider>
     </Router>
