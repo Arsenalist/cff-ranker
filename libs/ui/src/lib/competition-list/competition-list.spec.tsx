c0bf24f9547f33b50118706f78ba197ef95da2da
Zarar Siddiqi <zarars@gmail.com>
2020-12-16 11:55:03 -0500

    List competitions feature

--- libs/ui/src/lib/competition-list/competition-list.spec.tsx
+++ libs/ui/src/lib/competition-list/competition-list.spec.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import CompetitionList from './competition-list';
+import { act } from 'react-dom/test-utils';
+import MockAdapter from 'axios-mock-adapter';
+import '@testing-library/jest-dom';
+const mock = new MockAdapter(require('axios'));
 
 describe('CompetitionList', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<CompetitionList />);
-    expect(baseElement).toBeTruthy();
+  it('should render successfully', async() => {
+    mock.onGet('/api/competition').reply(200, [
+      {
+        name: 'big fencing tournament',
+        weapon: 'code'
+      }
+    ]);
+    await act(async () => {
+      render(<MemoryRouter><CompetitionList/></MemoryRouter>);
+    });
+    expect(screen.getByText(/big fencing tournament/i)).toBeInTheDocument();
+
   });
 });
--- libs/ui/src/lib/competition-list/competition-list.tsx
+++ libs/ui/src/lib/competition-list/competition-list.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
+import { Competition, CompetitionResults } from '@cff/api-interfaces';
+import axios from 'axios';
+import { List, ListItem } from '@material-ui/core';
+import ListItemText from '@material-ui/core/ListItemText';
 
 /* eslint-disable-next-line */
 export interface CompetitionListProps {}
@@ -10,10 +14,21 @@ const StyledCompetitionList = styled.div`
 `;
 
 export function CompetitionList(props: CompetitionListProps) {
+  const [competitions, setCompetitions] = useState<Competition[]>([])
+  useEffect(() => {
+    axios.get('/api/competition').then(response => {
+      setCompetitions(response.data)
+    });
+  }, []);
+
   return (
-    <StyledCompetitionList>
-      <h1>Welcome to competition-list!</h1>
-    </StyledCompetitionList>
+    <List>
+      {competitions && competitions.map((row: Competition) =>
+      <ListItem>
+        <ListItemText primary={row.name} secondary={row.code} />
+      </ListItem>
+      )}
+    </List>
   );
 }
 
