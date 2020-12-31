e0e3429c64c28412e95754b5266da912b5f72e2f
Zarar Siddiqi <zarars@gmail.com>
2020-12-31 15:47:05 -0500

    Move non-exported functions to bottom.

--- libs/ranking-algo/src/lib/ranking-algo.ts
+++ libs/ranking-algo/src/lib/ranking-algo.ts
@@ -1,22 +1,5 @@
 import { AgeCategory, CompetitionParticipant, PlayerClass, PlayerClassification } from '@cff/api-interfaces';
 
-function minimumForce(ageCategory: AgeCategory) {
-  const minimums = {
-    [AgeCategory.Open]: 30,
-    [AgeCategory.Masters]: 20,
-    [AgeCategory.Junior]: 20,
-    [AgeCategory.Cadet]: 10
-  }
-  return minimums[ageCategory]
-}
-
-
-function emptyPlayerClassCountMap() {
-  return Object.values(PlayerClass).reduce((accumulator, currentValue) => {
-    return { ...accumulator, [currentValue]: 0 };
-  }, {});
-}
-
 export function calculateForce(participants: CompetitionParticipant[], classification: PlayerClassification[], ageCategory: AgeCategory): number {
   const classMap = emptyPlayerClassCountMap();
   for (const p of participants) {
@@ -30,6 +13,22 @@ export function calculateForce(participants: CompetitionParticipant[], classific
 export function calculatePointsForParticipant(place: number, force: number, numberOfParticipants: number): number {
   const actual =  force * (1.006 - (Math.log10(place) / Math.log10(numberOfParticipants)))
   return roundToOneDecimal(actual)
+}
+
+function minimumForce(ageCategory: AgeCategory) {
+  const minimums = {
+    [AgeCategory.Open]: 30,
+    [AgeCategory.Masters]: 20,
+    [AgeCategory.Junior]: 20,
+    [AgeCategory.Cadet]: 10
+  }
+  return minimums[ageCategory]
+}
+
+function emptyPlayerClassCountMap() {
+  return Object.values(PlayerClass).reduce((accumulator, currentValue) => {
+    return { ...accumulator, [currentValue]: 0 };
+  }, {});
 }
 
 function roundToOneDecimal(x: number): number {
