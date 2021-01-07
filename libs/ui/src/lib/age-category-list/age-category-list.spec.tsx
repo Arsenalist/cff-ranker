f1512963cf117990b2780401064304f68ab99443
Zarar Siddiqi <zarars@gmail.com>
2021-01-07 16:36:10 -0500

    AgeCategory inline update UI #19

--- libs/ui/src/lib/age-category-list/age-category-list.spec.tsx
+++ libs/ui/src/lib/age-category-list/age-category-list.spec.tsx
@@ -11,19 +11,34 @@ const mock = new MockAdapter(require('axios'));
 describe('AgeCategoryList', () => {
   beforeEach(() => {
     mock.reset()
-    mock.onGet('/api/age-category').reply(200, [
+    mock.onGet('/api/age-category').replyOnce(200, [
       {
+        _id: '1',
         name: 'Senior',
         code: 'senior',
         yearOfBirth: 2020
       },
       {
+        _id: '2',
         name: 'Junior',
         code: 'junior',
         yearOfBirth: 2019
       }
 
-    ]);
+    ]).onGet('/api/age-category').replyOnce(200, [
+      {
+        _id: '1',
+        name: 'Senior',
+        code: 'senior',
+        yearOfBirth: 2020
+      },
+      {
+        _id: '2',
+        name: 'JuniorEdited',
+        code: 'junior-edited',
+        yearOfBirth: 2025
+      },
+    ]).onPost('/api/age-category').reply(200);
   })
   it('should show all age categories', async () => {
     await act(async () => {
@@ -35,5 +50,23 @@ describe('AgeCategoryList', () => {
     expect(screen.getByText(/Junior/)).toBeInTheDocument();
     expect(screen.getByText(/junior/)).toBeInTheDocument();
     expect(screen.getByText(/2019/)).toBeInTheDocument();
+  });
+  it('category is allowed to be edited', async () => {
+    await act(async () => {
+      render(<MemoryRouter><AgeCategoryList/></MemoryRouter>);
+    });
+    await act(async () => {
+      await userEvent.click(screen.getByTestId("edit-button-2"));
+      await fireEvent.change(screen.getByTestId("name-2"), {target: {value: "JuniorEdited"}});
+      await fireEvent.change(screen.getByTestId("code-2"), {target: {value: "juniorEdited"}});
+      await fireEvent.change(screen.getByTestId("yearOfBirth-2"), {target: {value: "2025"}});
+      await userEvent.click(screen.getByTestId("save-button-2"));
+    });
+    expect(screen.getByText(/Senior/)).toBeInTheDocument();
+    expect(screen.getByText(/senior/)).toBeInTheDocument();
+    expect(screen.getByText(/2020/)).toBeInTheDocument();
+    expect(screen.getByText(/JuniorEdited/)).toBeInTheDocument();
+    expect(screen.getByText(/junior-edited/)).toBeInTheDocument();
+    expect(screen.getByText(/2025/)).toBeInTheDocument();
   });
 });
--- libs/ui/src/lib/age-category-list/age-category-list.tsx
+++ libs/ui/src/lib/age-category-list/age-category-list.tsx
@@ -2,19 +2,74 @@ import React, { useEffect, useState } from 'react';
 
 import { AgeCategory } from '@cff/api-interfaces';
 import axios from 'axios';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-
-/* eslint-disable-next-line */
-export interface AgeCategoryListProps {}
+import { IconButton, Table, TableBody, TableCell, TableHead, TableRow, TextField } from '@material-ui/core';
 
 export function AgeCategoryList() {
   const [ageCategories, setAgeCategories] = useState<AgeCategory[]>([])
+  const [editMode, setEditMode] = useState<{_id: null | string, editMode: null | boolean} >({_id: null, editMode: null});
+  const [updated, setUpdated] = useState({});
+  const [reload, setReload] = useState(0)
   useEffect(() => {
-    console.log("here")
     axios.get('/api/age-category').then(response => {
+      response.data.forEach(c => {
+        setEditMode(state => ({...state, [c._id]: false}))
+        setUpdated(state => ({ ...state, [c._id]: c }));
+      })
       setAgeCategories(response.data)
     });
-  }, [])
+  }, [reload])
+
+  const CustomTableCell = ({ row, name, onChange }) => {
+    return (
+      <TableCell>
+        {editMode[row._id] ? (
+          <TextField
+            inputProps={{ "data-testid": `${name}-${row["_id"]}` }}
+            value={row[name]}
+            name={name}
+            onChange={e => onChange(e, row)}
+          />
+        ) : (
+          row[name]
+        )}
+      </TableCell>
+    );
+  };
+
+  const onToggleEditMode = id => {
+    setEditMode(state => ({...state, [id]: !state[id]}))
+  };
+
+  const onChange = (e, row) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setUpdated(state => ({ ...state, [row._id]: { ...row, [name]: value } }));
+  };
+
+  const onSave = (id) => {
+    const newRows = ageCategories.map(row => {
+      if (row._id === id) {
+        return updated[id]
+      } else {
+        return row
+      }
+    });
+    setAgeCategories(newRows)
+    axios.post('/api/age-category', updated[id]).then(response => setReload(state => state + 1) )
+  };
+
+  const onRevert = id => {
+    ageCategories.forEach(row => {
+      if (row._id === id) {
+        setUpdated(state => ({ ...state, [row._id]: row }))
+      }
+    });
+    onToggleEditMode(id);
+  };
+  if (!ageCategories) {
+    return <div>Loading...</div>
+  }
+
   return (
       <Table>
         <TableHead>
@@ -26,17 +81,38 @@ export function AgeCategoryList() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {ageCategories.map(c =>
-            <TableRow key={c.code}>
+          {ageCategories.map(row =>
+            <TableRow key={row._id}>
               <TableCell>
-                {c.name}
+                {editMode[row._id] ? (
+                  <>
+                    <IconButton
+                      aria-label="done"
+                      data-testid={`save-button-${row["_id"]}`}
+                      onClick={() => onSave(row._id)}
+                    >
+                      Save
+                    </IconButton>
+                    <IconButton
+                      aria-label="revert"
+                      onClick={() => onRevert(row._id)}
+                    >
+                      Cancel
+                    </IconButton>
+                  </>
+                ) : (
+                  <IconButton
+                    aria-label="edit"
+                    data-testid={`edit-button-${row["_id"]}`}
+                    onClick={() => onToggleEditMode(row._id)}
+                  >
+                    Edit
+                  </IconButton>
+                )}
               </TableCell>
-              <TableCell>
-                {c.code}
-              </TableCell>
-              <TableCell>
-                {c.yearOfBirth}
-              </TableCell>
+              <CustomTableCell {...{ row, name: "code", onChange }} />
+              <CustomTableCell {...{ row, name: "name", onChange }} />
+              <CustomTableCell {...{ row, name: "yearOfBirth", onChange }} />
             </TableRow>
           )}
         </TableBody>
