3de573b26b52f9bf9a1aa681fd0a37d35cec1652
Zarar Siddiqi <zarars@gmail.com>
2021-01-20 16:03:45 -0500

    Update Mongo connection.

--- apps/api/src/db/mongo-connection.ts
+++ apps/api/src/db/mongo-connection.ts
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function openMongo() {
-  mongoose.connect('mongodb+srv://web:3Juqrtx2TWVZU4Fu@cluster0.a4h6o.mongodb.net/cffranker?retryWrites=true&w=majority', { useNewUrlParser: true });
+  mongoose.connect('mongodb+srv://web:vxhxbwn3i3bXAk5@cluster0.lylny.mongodb.net/ranker?retryWrites=true&w=majority', { useNewUrlParser: true });
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
