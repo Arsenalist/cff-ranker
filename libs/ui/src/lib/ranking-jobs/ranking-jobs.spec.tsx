79770a4cd82276fafc5199165a284ad6d78cd271
Zarar Siddiqi <zarars@gmail.com>
2021-01-20 09:29:28 -0500

    Show ranking jobs #20

---
+++ libs/ui/src/lib/ranking-jobs/ranking-jobs.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import RankingJobs from './ranking-jobs';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import MockAdapter from 'axios-mock-adapter';
+import { RankingJob } from '@cff/api-interfaces';
+import '@testing-library/jest-dom';
+
+const mock = new MockAdapter(require('axios'));
+
+const rankingJob1: RankingJob = { _id: '1', user: 'someUser1', dateGenerated: new Date() };
+const rankingJob2: RankingJob = { _id: '2', user: 'someUser2', dateGenerated: new Date() };
+
+describe('RankingJobs', () => {
+  beforeEach(() => {
+    mock.reset()
+  });
+  it('shows ranking jobs', async () => {
+    mock.onGet('/api/rankings/jobs').replyOnce(200, [rankingJob1])
+    await act(async () => {
+      await render(<RankingJobs/>);
+    });
+    expect(screen.getByText(rankingJob1.user)).toBeInTheDocument();
+  });
+
+  it('ranking job is added afer calling rank', async () => {
+    mock.onGet('/api/rankings/jobs').replyOnce(200, [rankingJob1]).
+         onGet('/api/rankings/jobs').replyOnce(200, [rankingJob1, rankingJob2])
+    mock.onGet('/api/rank').reply(200)
+    await act(async () => {
+      await render(<RankingJobs/>);
+      await userEvent.click(screen.getByTestId("rank-button"));
+    });
+    expect(screen.getByText(rankingJob1.user)).toBeInTheDocument();
+    expect(screen.getByText(rankingJob2.user)).toBeInTheDocument();
+  });
+});
---
+++ libs/ui/src/lib/ranking-jobs/ranking-jobs.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Button, TableContainer, Table, TableBody, Paper, TableRow, TableCell, TableHead } from '@material-ui/core';
+import { RankingJob } from '@cff/api-interfaces';
+
+export function RankingJobs() {
+  const history = useHistory();
+  const [rankingJobs, setRankingJobs] = useState<RankingJob[]>([])
+  const [reload, setReload] = useState(0)
+  useEffect(() => {
+    axios.get('/api/rankings/jobs').then(response => {
+      setRankingJobs(response.data)
+    });
+  }, [reload])
+
+  const rank = () => {
+    axios.get('/api/rank').then(response => {
+      setReload(r => r + 1)
+    });
+  }
+
+  const showJobDetails = (id) => {
+    history.push("/rankings/jobs/" + id)
+  }
+
+  return (
+    <>
+      (<Button variant="contained" color="primary" data-testid="rank-button" onClick={() => rank()}>Ranking</Button>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow key="header">
+              <TableCell>User</TableCell>
+              <TableCell>Date Generated</TableCell>
+              <TableCell> </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              rankingJobs.map(row => (
+                <TableRow key={row._id}>
+                  <TableCell>{row.user}</TableCell>
+                  <TableCell>{row.dateGenerated}</TableCell>
+                  <TableCell>
+                    <Button variant="outlined" color="primary" data-testid="rank" onClick={() => showJobDetails(row._id)}>Ranking</Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default RankingJobs;
