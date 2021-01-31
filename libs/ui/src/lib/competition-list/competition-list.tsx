5ecac8aa7c2b109704c30d282127229bdd8aa6b3
Zarar Siddiqi <zarars@gmail.com>
2021-01-30 20:45:46 -0500

    Formatting

--- libs/ui/src/lib/competition-list/competition-list.tsx
+++ libs/ui/src/lib/competition-list/competition-list.tsx
@@ -25,98 +25,100 @@ import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
 
 export function CompetitionList() {
-  const [competitions, setCompetitions] = useState<Competition[]>([])
-  const [openAddDialog, setAddDialogOpen] = useState(false)
-  const [reload, setReload] = useState(0)
+  const [competitions, setCompetitions] = useState<Competition[]>([]);
+  const [openAddDialog, setAddDialogOpen] = useState(false);
+  const [reload, setReload] = useState(0);
   const { register, handleSubmit } = useForm<Competition>();
   useEffect(() => {
     axios.get('/api/competition').then(response => {
-      setCompetitions(response.data)
+      setCompetitions(response.data);
     });
   }, [reload]);
 
   const onSubmit = (data: Competition) => {
     axios.put('/api/competition', data).then(response => {
-      setReload(reload + 1)
-      setAddDialogOpen(false)
+      setReload(reload + 1);
+      setAddDialogOpen(false);
     });
-  }
+  };
 
   const onDelete = (code: string) => {
-    axios.delete('/api/competition', {data: {code: code}}).then(() => {
-      setReload(reload + 1)
-      setAddDialogOpen(false)
+    axios.delete('/api/competition', { data: { code: code } }).then(() => {
+      setReload(reload + 1);
+      setAddDialogOpen(false);
     });
-  }
+  };
 
   return (
     <div>
-      <Button variant="contained" color="primary" data-testid="add-button" onClick={() => setAddDialogOpen(true)}>Add Competition</Button>
+      <Button variant="contained" color="primary" data-testid="add-button" onClick={() => setAddDialogOpen(true)}>Add
+        Competition</Button>
       <TableContainer component={Paper}>
         <Table>
-      <TableHead>
-        <TableRow key="header">
-          <TableCell>Name</TableCell>
-          <TableCell>Code</TableCell>
-          <TableCell>Zone</TableCell>
-          <TableCell>Delete</TableCell>
-        </TableRow>
-      </TableHead>
+          <TableHead>
+            <TableRow key="header">
+              <TableCell>Name</TableCell>
+              <TableCell>Code</TableCell>
+              <TableCell>Zone</TableCell>
+              <TableCell>Delete</TableCell>
+            </TableRow>
+          </TableHead>
           <TableBody>
-      {competitions && competitions.map((row: Competition) =>
-      <TableRow key={row.code}>
-        <TableCell scope="row">{row.name}</TableCell>
-        <TableCell scope="row">{row.code}</TableCell>
-        <TableCell scope="row">{row.zone}</TableCell>
-        <TableCell scope="row">
-          <IconButton edge="end" aria-label="delete" data-testid="delete-button" onClick={() => onDelete(row.code)}>
-            <DeleteIcon />
-          </IconButton>
-        </TableCell>
-      </TableRow>
-      )}
+            {competitions && competitions.map((row: Competition) =>
+              <TableRow key={row.code}>
+                <TableCell scope="row">{row.name}</TableCell>
+                <TableCell scope="row">{row.code}</TableCell>
+                <TableCell scope="row">{row.zone}</TableCell>
+                <TableCell scope="row">
+                  <IconButton edge="end" aria-label="delete" data-testid="delete-button"
+                              onClick={() => onDelete(row.code)}>
+                    <DeleteIcon/>
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
-      <Dialog open={openAddDialog}  onClose={() => setAddDialogOpen(false)} aria-labelledby="form-dialog-title">
+      <Dialog open={openAddDialog} onClose={() => setAddDialogOpen(false)} aria-labelledby="form-dialog-title">
         <form onSubmit={handleSubmit(onSubmit)}>
-        <DialogTitle >Add a Competition</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            inputProps={{ "data-testid": "name" }}
-            label="Name"
-            name="name"
-            inputRef={register}
-            type="text"
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            inputProps={{ "data-testid": "code" }}
-            label="Code"
-            name="code"
-            inputRef={register}
-            type="text"
-            fullWidth
-          />
-          <br/><br/>
-          <FormLabel component="legend">Zone</FormLabel>
-          <RadioGroup aria-label="gender" name="zone" defaultValue={CompetitionZone.cff}>
-            {Object.values(CompetitionZone).map(value =>
-              (<FormControlLabel control={<Radio value={value}  inputRef={register} />} label={value} />)
-            )}
-          </RadioGroup>
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary">
-            Cancel
-          </Button>
-          <Button type="submit" color="primary" data-testid="add-button-confirm">
-            Add
-          </Button>
-        </DialogActions>
+          <DialogTitle>Add a Competition</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              inputProps={{ 'data-testid': 'name' }}
+              label="Name"
+              name="name"
+              inputRef={register}
+              type="text"
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              inputProps={{ 'data-testid': 'code' }}
+              label="Code"
+              name="code"
+              inputRef={register}
+              type="text"
+              fullWidth
+            />
+            <br/><br/>
+            <FormLabel component="legend">Zone</FormLabel>
+            <RadioGroup aria-label="gender" name="zone" defaultValue={CompetitionZone.cff}>
+              {Object.values(CompetitionZone).map(value =>
+                (<FormControlLabel control={<Radio value={value} inputRef={register}/>} label={value}/>)
+              )}
+            </RadioGroup>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary" data-testid="add-button-confirm">
+              Add
+            </Button>
+          </DialogActions>
         </form>
       </Dialog>
     </div>
