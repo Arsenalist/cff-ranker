d1c8c5610661fddcbb4032ae3c1d61e3a41e5a9b
Zarar Siddiqi <zarars@gmail.com>
2021-02-05 13:07:36 -0500

    Differnt baseUrl for production.

--- apps/ranker/src/app/auth.tsx
+++ apps/ranker/src/app/auth.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
+import { environment } from '../environments/environment';
 
 export function Auth() {
   const { getAccessTokenSilently } = useAuth0()
   axios.interceptors.request.use(function (config) {
+    config.baseURL = environment.apiBaseUrl;
     (async () => {
       const token = await getAccessTokenSilently();
       config.headers.Authorization = `Bearer ${token}`;
--- apps/ranker/src/environments/environment.prod.ts
+++ apps/ranker/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiBaseUrl: 'https://cff-ranker-api.herokuapp.com'
 };
